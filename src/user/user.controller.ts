eef0f680b5963ca9e4634e63c230c9337a813da7
mind.hunter <mey3am.m69@gmail.com>
2024-06-27 18:19:52 +0330

    feat(develop): usercontroller doc

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,33 +10,51 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({
+    summary:
+      'Create a new instance of the model and persist it into the data source.',
+  })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @ApiOperation({
+    summary:
+      'Find all instances of the model matched by filter from the data source.',
+  })
   @Get()
   findAll(query: any) {
     return this.userService.findAll(query);
   }
 
+  @ApiOperation({
+    summary: 'Find a model instance by {{id}} from the data source.',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  @ApiOperation({
+    summary:
+      'Patch attributes for a model instance and persist it into the data source.',
+  })
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
 
+  @ApiOperation({
+    summary: "Delete a model instance by '{id}' from the data source.",
+  })
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.userService.remove(id);
